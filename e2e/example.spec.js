f2432d221f4dc8cdf00410e57864dedde6db7786
anis3669 <bastolaanis1230@gmail.com>
2026-09-24 17:42:15 +0545

    added test case

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -1,8 +1,13 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
 
-test("application loads", async ({ page }) => {
-    await page.goto("/");
+test("admin can login successfully", async ({ page }) => {
+    await page.goto("http://127.0.0.1:8000/login");
 
-    await expect(page).toHaveTitle("Laravel Vite");
+    await page.getByLabel("Email").fill("admin@gmail.com");
+    await page.getByLabel("Password").fill("password");
+
+    await page.getByRole("button", { name: "Sign In" }).click();
+
+    await expect(page).toHaveURL("http://127.0.0.1:8000/products");
 });
